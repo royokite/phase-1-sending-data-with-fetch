16fd4f4156118c92efd96a7cf9e08c1cbc406eb2
RoyOkite <royokite@gmail.com>
2022-08-28 14:05:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your code here
+// Sending data with fetch!
+
+function submitData(userName, userEmail) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail,
+        })
+    })
+    .then(response => response.json())
+    .then(user => document.body.innerHTML = `${user.id}`)
+    .catch(error => document.body.innerHTML = error.message);
+}
